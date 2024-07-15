478acb5f9e36ac30ec2849f064c27987bf16b563
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-07-16 02:20:24 +0300

    add webview that later will be used to extract content from client-side rendered js app

--- app/src/main/java/com/mykola2312/outagefetch/MainActivity.kt
+++ app/src/main/java/com/mykola2312/outagefetch/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.mykola2312.outagefetch
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.mykola2312.outagefetch.ui.theme.OutageFetchTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    FetchScreen()
                 }
             }
         }
@@ -30,17 +36,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ExtractWebView() {
+    val url = "https://2ip.ua"
+
+    AndroidView(factory = {
+        WebView(it).apply {
+            this.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            this.webViewClient = ExtractorClient()
+        }
+    }, update = {
+        it.loadUrl(url);
+    })
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OutageFetchTheme {
-        Greeting("Android")
+class ExtractorClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        return false
     }
+}
+
+@Composable
+fun FetchScreen() {
+    ExtractWebView()
+}
+
+@Preview
+@Composable
+fun FetchScreenPreview() {
+    FetchScreen()
 }
